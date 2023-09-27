a119c338ead042427978c630463b463bd546e1b1
arthur rosa menezes costa <99087181+deltafox28@users.noreply.github.com>
2023-09-27 17:38:16 -0300

    Update 33_SELECT_MY.py

--- 33_SELECT_MY.py
+++ 33_SELECT_MY.py
@@ -3,34 +3,42 @@
 # SELECT_MY.py
 
 """
-  Nesse arquivo, vamos 
-  fazer um teste de
-  update e delete ao banco 
-  de dados do MySQL.
-
-  Modificado em 27 de março de 2017
-  por Vitor Mazuco (vitor.mazuco@gmail.com)
+  Nesse arquivo, vamos fazer um teste de consulta 
+  ao banco de dados MySQL.
 """
+
 import sys
+import mysql.connector
 
 try:
-	import MySQLdb
-except:
-    sys.exit("[!] Por favor, intale a biblioteca mysqldb com o comando: sudo apt-get install python-mysqldb")
+    con = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        database="projeto",
+        password="123456"
+    )
+    cur = con.cursor()
 
-try:
-	con = MySQLdb.connect(host="127.0.0.1", user="root", db="projeto", passwd="123456") 
-	cur = con.cursor()
-	cur.execute("select * from cliente")
-	print "Primeiro registro: ",cur.fetchone()
-	print "Todos os registros: ",cur.fetchall()
-except Exception as e:
-	print "Erro: %s"%e
-	con.rollback # Rollback feito de propósito!
+    # Executar a consulta SQL
+    cur.execute("SELECT * FROM cliente")
+
+    # Recuperar o primeiro registro
+    first_record = cur.fetchone()
+    print("Primeiro registro:", first_record)
+
+    # Recuperar todos os registros restantes
+    all_records = cur.fetchall()
+    print("Todos os registros:", all_records)
+
+except mysql.connector.Error as e:
+    print("Erro:", e)
+    con.rollback()  # Rollback em caso de erro
+
 finally:
-	print "Finalizando a conexão com o banco de dados"
-	cur.close()
-	con.close()
+    print("Finalizando a conexão com o banco de dados")
+    if con.is_connected():
+        cur.close()
+        con.close()
 
 
 
