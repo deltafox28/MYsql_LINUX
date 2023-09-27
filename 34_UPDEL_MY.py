12fc4d31597f6eb74da0efa8f7c5d78f1d80778f
arthur rosa menezes costa <99087181+deltafox28@users.noreply.github.com>
2023-09-27 17:39:33 -0300

    Update 34_UPDEL_MY.py

--- 34_UPDEL_MY.py
+++ 34_UPDEL_MY.py
@@ -3,34 +3,40 @@
 # UPDEL_MY.py
 
 """
-  Nesse arquivo, vamos 
-  fazer um teste de
-  update e delete ao banco 
-  de dados do MySQL.
-
-  Modificado em 27 de março de 2017
-  por Vitor Mazuco (vitor.mazuco@gmail.com)
+  Nesse arquivo, vamos fazer um teste de
+  atualização e exclusão no banco de dados MySQL.
 """
 
-try:
-	import MySQLdb
-except:
-    sys.exit("[!] Por favor, intale a biblioteca mysqldb com o comando: sudo apt-get install python-mysqldb")
+import sys
+import mysql.connector
 
 try:
-	con = MySQLdb.connect(host="127.0.0.1", user="root", db="projeto", passwd="123456") 
-	cur = con.cursor()
-	cur.execute("update cliente set nome='Alberto' where id=1")
-	print "Atualização feita com sucesso"
-	cur.execute("delete from cliente where id=2")
-	con.commit()
-	print "Registro apagado com sucesso"
-except Exception as e:
-	print "Erro: %s"%e
-	con.rollback
+    con = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        database="teste",
+        password="123456"
+    )
+    cur = con.cursor()
+
+    # Atualização (UPDATE)
+    cur.execute("UPDATE cliente SET nome='Alberto' WHERE id=1")
+    con.commit()  # Commit após a atualização
+    print("Atualização feita com sucesso")
+
+    # Exclusão (DELETE)
+    cur.execute("DELETE FROM cliente WHERE id=2")
+    con.commit()  # Commit após a exclusão
+    print("Registro apagado com sucesso")
+
+except mysql.connector.Error as e:
+    print("Erro:", e)
+    con.rollback()  # Rollback em caso de erro
+
 finally:
-	print "Finalizando a conexão com o banco de dados"
-	cur.close()
-	con.close()
+    print("Finalizando a conexão com o banco de dados")
+    if con.is_connected():
+        cur.close()
+        con.close()
 
 
